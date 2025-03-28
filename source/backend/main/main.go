6e8034bd53de47b7563acee0d097d8f1a36b8d70
Ojas <ojasat5@gmail.com>
2025-03-28 14:43:31 -0400

    feat(backend) : added sharedmem reading logic from go

--- source/backend/main/main.go
+++ source/backend/main/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
 const (
 	SHM_NAME    = "JIT_CORE_OPEN_MEM"
 	SHM_SIZE    = 16384
@@ -15,7 +21,34 @@ type SharedQueue struct {
 	Mutex   uintptr
 }
 
+func main() {
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	openFileMapping := kernel32.NewProc("OpenFileMappingW")
+	mapViewOfFile := kernel32.NewProc("MapViewOfFile")
 
-kernel32 := syscall.NewLazyDLL("kernel32.dll")
-openFileMapping := kernel32.NewProc("OpenFileMappingW")
-mapViewOfFile := kernel32.NewProc("MapViewOfFile")
+	hMap, _, _ := openFileMapping.Call(0x0002, 0, uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(SHM_NAME))))
+	if hMap == 0 {
+		fmt.Println("Failed to open shared memory")
+		return
+	}
+
+	// Map shared memory to our process
+	ptr, _, _ := mapViewOfFile.Call(hMap, 0x0004, 0, 0, SHM_SIZE)
+	if ptr == 0 {
+		fmt.Println("Failed to map shared memory")
+		return
+	}
+
+	queue := (*SharedQueue)(unsafe.Pointer(ptr))
+
+	fmt.Println("Reading packets from shared memory queue...")
+
+	for {
+		if queue.Count > 0 {
+			packet := queue.Packets[queue.Head]
+			fmt.Printf("Packet from queue: %s\n", string(packet[:]))
+			queue.Head = (queue.Head + 1) % QUEUE_SIZE
+			queue.Count--
+		}
+	}
+}
